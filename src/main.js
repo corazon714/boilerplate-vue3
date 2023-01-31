bc7b1ec1dc166af421a518e258c690c70f7abfe1
Mert Kaya <mert.kaya.98@protonmail.com>
2023-01-31 14:22:36 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import "./index.css"
+
+import { router } from "./router";
+
+const pinia = createPinia()
+
+createApp(App).use(pinia).use(router).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1 class="text-5xl text-gray-800">VUE 3 BOILERPLATE WITH TAILWINDCSS + NAIVEUI</h1>
+    <n-button>test button</n-button>
+    <p class="text-3xl">In this boilerplate; vue3 setted up within use of: tailwindcss, naive ui, vue-router, pinia.</p>
+    <p class="text-xl">Naive UI setted up with auto-import. So you dont have to import every UI Element you wanna use.</p>
+</template>
